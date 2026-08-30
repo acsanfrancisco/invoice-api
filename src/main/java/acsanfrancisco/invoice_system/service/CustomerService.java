abed1f8f7ad404bad47050a50f8eb3bdc891d8ce
João Francisco Santiago <joaosannetof@gmail.com>
2026-08-30 20:54:41 -0300

    refactor(customer): rename CustomerSpecifications to CustomerSpecification

--- src/main/java/acsanfrancisco/invoice_system/service/CustomerService.java
+++ src/main/java/acsanfrancisco/invoice_system/service/CustomerService.java
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.UUID;
 
-import static acsanfrancisco.invoice_system.entity.specification.CustomerSpecifications.*;
+import static acsanfrancisco.invoice_system.entity.specification.CustomerSpecification.*;
 
 @Service
 @RequiredArgsConstructor
